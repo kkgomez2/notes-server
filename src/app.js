b3d4cd1d4e6b5554bb3f890e3c38878a7bb3b2e7
kkgomez2 <kkristoffergomez@gmail.com>
2025-06-29 16:44:41 -0700

    Connecting to database

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 import express from "express";
 import notesRoutes from "./routes/notesRoutes.js"
+import { connectDB } from "./config/db.js";
+import "dotenv/config";
 
 const app = express();
+const PORT = process.env.PORT || 3001;
+
+connectDB();
 
 app.use("/api/notes", notesRoutes);
 
-app.listen(3001, () => {
+app.listen(PORT, () => {
   console.log("Server started");
   console.log("Listening on localhost:3001");
 })
